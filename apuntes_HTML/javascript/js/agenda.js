d15dbb1b99514849efe72dc66a8855448a4ddbeb
oscarcm99 <gorri0302@gmail.com>
2018-05-22 19:09:49 +0200

    agenda

--- apuntes_HTML/javascript/js/agenda.js
+++ apuntes_HTML/javascript/js/agenda.js
@@ -1,5 +1,42 @@
 class  Contacto{
-	constructor(nombre,direccion,telefono){
-
+	constructor(nombre,telefono,direccion){
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
 	}
+	cogerNombre(nom){
+		this.nombre = nom;
+	}
+	cogerTelefono(tel){
+		this.telefono = tel;
+	}
+	cogerDireccion(dir){
+		this.direccion = dir;
+	}
+	EscribirN(){
+    	var rn;
+    	rn = document.getElementById('resultadoañadido');
+    	rn.innerHTML += "nombre: " +this.nombre + "<br>" +
+    					"telefono: " +this.direccion + "<br>" +
+    					"direccion: " +this.telefono + "<br>";
+	}
+}
+function AñadirContacto() {
+	// body...
+	var vn, vd, vt;
+	var miTabla = new Array(4);
+	vn = document.getElementById('nombre').value;
+	vt = document.getElementById('telefono').value;
+	vd = document.getElementById('direccion').value;
+	for(var i = 0; i<miTabla.length; i++){
+		miTabla[i].cogerNombre(vn);
+		miTabla[i].cogerTelefono(vt);
+		miTabla[i].cogerDireccion(vd);
+		miTabla[i].EscribirN();
+	}
+	
+	
+}
+function ModificarContacto(){
+
 }
