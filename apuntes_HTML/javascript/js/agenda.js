e21d9796f2f25f6b81ad61ff374caac33841c71d
oscarcm99 <gorri0302@gmail.com>
2018-05-24 20:40:43 +0200

    Haciendo agenda

--- apuntes_HTML/javascript/js/agenda.js
+++ apuntes_HTML/javascript/js/agenda.js
@@ -1,5 +1,5 @@
 class  Contacto{
-	constructor(nombre,telefono,direccion){
+	constructor(nombre, telefono, direccion){
 		this.nombre = nombre;
 		this.telefono = telefono;
 		this.direccion = direccion;
@@ -13,18 +13,32 @@ class  Contacto{
 	cogerDireccion(dir){
 		this.direccion = dir;
 	}
+	cambiarNombre(nuevonom){
+		this.nombre = nuevonom;
+	}
+	cambiarTelefono(nuevotel){
+		this.telefono = nuevotel;
+	}
+	cambiarDireccion(nuevodir){
+		this.direccion = nuevodir;
+	}
 	EscribirN(){
     	var rn;
     	rn = document.getElementById('resultadoañadido');
-    	rn.innerHTML += "nombre: " +this.nombre + "<br>" +
+    	rn.innerHTML = "nombre: " +this.nombre + "<br>" +
     					"telefono: " +this.direccion + "<br>" +
     					"direccion: " +this.telefono + "<br>";
 	}
 }
+var miTabla = new Array(5),vn;
 function AñadirContacto() {
 	// body...
-	var vn, vd, vt;
-	var miTabla = new Array(4);
+	var  vd, vt;
+	miTabla[0] = new Contacto(vn, vd, vt);
+	miTabla[1] = new Contacto(vn, vd, vt);
+	miTabla[2] = new Contacto(vn, vd, vt);
+	miTabla[3] = new Contacto(vn, vd, vt);
+	miTabla[4] = new Contacto(vn, vd, vt);
 	vn = document.getElementById('nombre').value;
 	vt = document.getElementById('telefono').value;
 	vd = document.getElementById('direccion').value;
@@ -32,11 +46,27 @@ function AñadirContacto() {
 		miTabla[i].cogerNombre(vn);
 		miTabla[i].cogerTelefono(vt);
 		miTabla[i].cogerDireccion(vd);
-		miTabla[i].EscribirN();
 	}
+	for(var i = 0; i<miTabla.length; i++){
+		miTabla[0].EscribirN();
+	}
+	
 	
 	
 }
 function ModificarContacto(){
-
+	var vant,nnom,ntel,ndir;
+	vant = document.getElementById('nomcamb').value;
+	nnom = document.getElementById('nommod').value;
+	ntel = document.getElementById('telmod').value;
+	ndir = document.getElementById('dirmod').value;
+for (var i = 0; i <miTabla.length; i++)
+	if (vant === vn) {
+		miTabla[i].cambiarNombre(nnom);
+		miTabla[i].cambiarTelefono(ntel);
+		miTabla[i].cambiarDireccion(ndir);
+	}
+}
+function BuscarContacto() {
+	
 }
